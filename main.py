8f892313d0669af4ca6a50685b861892e4a5484f
Beatriz Cruz <mllebia@live.com>
2024-02-07 15:42:06 -0300

    testing-scraping

---
+++ main.py
@@ -0,0 +1,8 @@
+import lxml.html
+import requests
+
+
+html = requests.get('https://store.steampowered.com/explore/new/')
+doc = lxml.html.fromstring(html.content)
+
+new_releases = doc.xpath('//div[@id="tab_newreleases_content"]')[0]
